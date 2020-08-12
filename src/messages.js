49a30bc003ef80793c6ef4cee490544b5309e4ee
david145noone <david145noone@gmail.com>
2020-08-12 16:40:10 +0100

    Added error message

--- src/messages.js
+++ src/messages.js
@@ -2,7 +2,6 @@ const Discord = require('discord.js');
 
 module.exports = {
     help: function(username) {
-
         const exampleEmbed = new Discord.MessageEmbed()
         .setColor('#0099ff')
         .setTitle('Hi there ' + username + '!')
@@ -11,29 +10,22 @@ module.exports = {
         .setThumbnail('https://i.imgur.com/wSTFkRM.png')
         .addFields(
             { name: '!accounts', value: 'Calls the accounts bot and lists the commands. '},
-            { name: '!check', value: 'Checks to see if you have a CompSoc account. (Not ready)'},
-            { name: '!request', value: 'Requests an account from CompSoc. (Also not ready)'},
+            { name: '!check [Must be done in a DM with Accounts Bot]', value: 'Checks to see if you have a CompSoc account. (Not ready)'},
+            { name: '!request [Must be done in a DM with Accounts Bot]', value: 'Requests an account from CompSoc. (Also not ready)'},
             { name: '\u200B', value: '\u200B' }
         )
         .setTimestamp();
 
         return exampleEmbed;
     },
-    checkIfAccount: function(username) {
+    notInDM: function(username, action) {
         const exampleEmbed = new Discord.MessageEmbed()
         .setColor('#8121C4')
         .setTitle('Hi there ' + username + '!')
         .setAuthor('NUIG Computer Society Account Bot', 'https://i.imgur.com/wSTFkRM.png', 'https://compsoc.ie/')
-        .setDescription('In order to check whether you have an account with us, please direct message Accounts Bot with your student number.')
+        .setDescription('The action: ' + action + ', can only be done in a DM with Accounts Bot and not in a public channel.')
         .setThumbnail('https://i.imgur.com/wSTFkRM.png')
-        .addFields(
-            { name: 'Note that:', value: 'The bot will delete these messages after 1 minute has passed for GDPR.'}
-        )
         .setTimestamp();
-
         return exampleEmbed;
-    },
-    requestAccount: function() {
-
     }
 };
